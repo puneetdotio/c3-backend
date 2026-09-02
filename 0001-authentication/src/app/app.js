33b9bbaff63f17acf51a13cdf289bdbc5e902186
PuneetVerma07 <pk2382849@gmail.com>
2026-09-02 16:42:40 +0530

    pract and feat: authentication implementation

--- 0001-authentication/src/app/app.js
+++ 0001-authentication/src/app/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
 const app = express();
 
@@ -7,25 +7,31 @@ app.use(express.json())
 
 app.get("/api", (req, res) => {
     res.status(200).json({
-        message: "Welcome to authentication api",
+        message: "Welcome to authentication API",
     })
 })
 
 app.post("/api/auth/register", (req, res) => {
-    const { email, name, password } = req.body;
+    try {
+        const { email, name, password } = req.body;
 
-    const token = jwt.sign({ email, name }, "ff2b7e5263ceeb79ea0d3092c3beb0ba")
+        const token = jwt.sign({ email, name }, "12e6e7be58ae7f2b55f453b3df789d7b")
 
-    res.status(201).json({
-        success: true,
-        message:"user created successfully",
-        data: {
-            user: {
-                email, name
-            },
-            token,
-        }
-    })
+        res.status(201).json({
+            success: true,
+            message: "user created successfully",
+            data: {
+                user: { email, name },
+                token,
+            }
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
 })
 
 export default app;
--- 0001-authentication/src/server.js
+++ 0001-authentication/src/server.js
@@ -1,7 +1,7 @@
-import app from "./app/app.js";
+import app from "./app/app.js"
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log("Server is running on port ", port)
+    console.log("Server is running on ", port)
 })
