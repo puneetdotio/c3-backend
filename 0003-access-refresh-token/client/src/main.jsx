70d67663a1590bccf0a64ef388a1d0fd21483ab3
PuneetVerma07 <pk2382849@gmail.com>
2026-09-13 13:49:12 +0530

    AuthProvider add

--- 0003-access-refresh-token/client/src/main.jsx
+++ 0003-access-refresh-token/client/src/main.jsx
@@ -1,9 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppRoutes from './app/routes/AppRoutes.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRoutes from "./app/routes/AppRoutes.jsx";
+import AuthProvider from "./features/auth/state/context/AuthProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
-  
-    <AppRoutes />
-  
-)
+createRoot(document.getElementById("root")).render(
+	<AuthProvider>
+		<AppRoutes />
+	</AuthProvider>,
+);
